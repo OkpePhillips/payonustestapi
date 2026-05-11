c641a25a3c4aa1c74de53b94e720d4bd0ff36d35
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2026-05-11 16:27:35 +0100

    dob resolved

--- api/services/payments.py
+++ api/services/payments.py
@@ -138,6 +138,7 @@ def create_fixed_virtual_account(data):
             "phone": customer.phone,
             "externalId": customer.external_id,
             "bvn": customer.bvn,
+            "dob": (customer.dob.strftime("%Y-%m-%d")),
             "address": {
                 "line1": address_data.get("line1"),
                 "line2": address_data.get("line2"),
@@ -147,7 +148,6 @@ def create_fixed_virtual_account(data):
                 "countryCode": address_data.get("country_code"),
             },
         },
-        "dob": (customer.dob.strftime("%Y-%m-%d")),
         "businessId": (settings.PAYONUS_BUSINESS_ID),
         "reference": str(transaction.reference),
     }
